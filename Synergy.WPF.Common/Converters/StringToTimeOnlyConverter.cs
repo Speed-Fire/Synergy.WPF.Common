f11a8de8e6878ea28caf65921aaed31ededac541
Vladislav Sidorovici <sidorvla@fit.cvut.cz>
2023-09-19 23:01:47 +0200

    Adding StringToTimeOnlyConverter.

---
+++ Synergy.WPF.Common/Converters/StringToTimeOnlyConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Synergy.WPF.Common.Converters
+{
+    public class StringToTimeOnlyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var time = (TimeOnly)value;
+
+            return time.ToString("hh:mm");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var str = (string)value;
+
+            return TimeOnly.Parse(str);
+        }
+    }
+}
