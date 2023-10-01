c5215dfe933b32f9eacf223f9151edff8b591b58
Vladislav Sidorovici <sidorvla@fit.cvut.cz>
2023-10-01 17:25:03 +0200

    Adding navigation extensions.

---
+++ Synergy.WPF.Common/Extensions/NavigationExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace Synergy.WPF.Common.Extensions
+{
+    public static class NavigationExtensions
+    {
+        public static bool NavigateNoJournal(this NavigationService service, Func<object> rootGetter)
+        {
+            JournalEntry entry = null;
+
+            service.Navigate(null);
+            if(service.CanGoBack)
+                entry = service.RemoveBackEntry();
+
+            if(service.Navigate(rootGetter.Invoke()))
+            {
+                return true;
+            }
+            else if(entry != null)
+            {
+                service.Navigate(entry);
+            }
+
+            return false;
+        }
+
+        public static bool NavigateNoJournal(this Frame frame, Func<object> rootGetter)
+        {
+            return frame.NavigationService.NavigateNoJournal(rootGetter);
+        }
+    }
+}
