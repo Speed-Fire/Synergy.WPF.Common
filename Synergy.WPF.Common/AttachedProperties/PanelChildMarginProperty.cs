4573f7719ca6fbbb0c8b1b8d78b51a76280cab32
Vladislav Sidorovici <sidorvla@fit.cvut.cz>
2023-11-04 15:45:26 +0100

    Adding some attached properties.

---
+++ Synergy.WPF.Common/AttachedProperties/PanelChildMarginProperty.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Synergy.WPF.Common.AttachedProperties
+{
+	/// <summary>
+	/// The PanelChildMargin attached property for setting specified margin to all <see cref="Panel"/> children.
+	/// </summary>
+	public class PanelChildMarginProperty : BaseAttachedProperty<PanelChildMarginProperty, string>
+	{
+		public override void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+		{
+			// Get the panel (grid typically)
+			var panel = (sender as Panel);
+
+			// Wait for panel to load
+			panel.Loaded += (s, ee) =>
+			{
+				// Loop each child
+				foreach (var child in panel.Children)
+					// Set it's margin to the given value
+					(child as FrameworkElement).Margin = (Thickness)(new ThicknessConverter().ConvertFromString(e.NewValue as string));
+			};
+		}
+	}
+}
