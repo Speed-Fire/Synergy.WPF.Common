83b7f67652401da2012e5fa10499e08c3c3429d0
Vladislav Sidorovici <sidorvla@fit.cvut.cz>
2023-11-07 13:56:24 +0100

    Adding boolean inversion converter.

---
+++ Synergy.WPF.Common/Converters/InvertBoolConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Synergy.WPF.Common.Converters
+{
+	public class InvertBoolConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is not bool b)
+				throw new ArgumentException("Value is not a boolean!");
+
+			return !b;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return Convert(value, targetType, parameter, culture);
+		}
+	}
+}
