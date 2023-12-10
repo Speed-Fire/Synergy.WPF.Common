951e06642ad2225c2b8e53548adf7d32b2f058de
Vladislav Sidorovici <sidorvla@fit.cvut.cz>
2023-12-10 19:04:31 +0100

    Adding converters.

---
+++ Synergy.WPF.Common/Converters/IsEqualConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Synergy.WPF.Common.Converters
+{
+	public class IsEqualConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if(parameter is null && value is null)
+				return true;
+
+			if(parameter is null ^ value is null)
+				return false;
+
+			return value.Equals(parameter);
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Synergy.WPF.Common/Converters/IsGreaterConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Synergy.WPF.Common.Converters
+{
+	public class IsGreaterConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (parameter is null && value is null)
+				return false;
+
+			if (value is null)
+				return false;
+
+			if(parameter is null)
+				return true;
+
+			return Comparer.Default.Compare(value, parameter) > 0;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
