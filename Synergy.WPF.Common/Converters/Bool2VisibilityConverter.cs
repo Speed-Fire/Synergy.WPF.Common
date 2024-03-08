543f021e287a4bc5203d2597b268288809f4ec09
Vladislav Sidorovici <sidorvla@fit.cvut.cz>
2024-03-08 14:26:08 +0100

    Fixed: - BoolVisibilityConverter
    Added:
    - ValueConverterGroup

--- Synergy.WPF.Common/Converters/Bool2VisibilityConverter.cs
+++ Synergy.WPF.Common/Converters/Bool2VisibilityConverter.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Data;
 
@@ -13,6 +9,9 @@ namespace Synergy.WPF.Common.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            if(value is null)
+                return Visibility.Collapsed;
+
             if(parameter is not null && parameter is string str)
             {
                 if(str.Equals("Invert"))
