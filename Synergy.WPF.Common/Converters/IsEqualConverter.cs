25f98a76e43f586f716f386451a4205683a280b1
Vladislav Sidorovici <sidorvla@fit.cvut.cz>
2024-04-30 19:32:14 +0200

    Updating converter.

--- Synergy.WPF.Common/Converters/IsEqualConverter.cs
+++ Synergy.WPF.Common/Converters/IsEqualConverter.cs
@@ -22,4 +22,24 @@ namespace Synergy.WPF.Common.Converters
 			throw new NotImplementedException();
 		}
 	}
+
+	public class IsEqualMultiConverter : IMultiValueConverter
+	{
+		public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+		{
+			var res = true;
+
+			for(int i = 1; i < values.Length && res; i++)
+			{
+				res &= values[i - 1].Equals(values[i]);
+			}
+
+			return res;
+		}
+
+		public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
